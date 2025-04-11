2475d462640f891a5dd0f3f04cf2dfcff55b07c2
CeliacByte <barriosurielivan@gmail.com>
2025-04-10 21:27:08 -0300

    Actualizacion con primera parte de los ejercicios realizada

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ const productos = [
     { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
     { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
     ];
+    
+//Parte1
 //Ejercicio 1
+console.log("\n"+productos[0].nombre+"\n"); // Remera
+
+//Ejercicio 2
+for (let index = 0; index < productos.length; index++) {
+    console.log(`Producto: ${productos[index].nombre} - Precio: ${productos[index].precio}`)
+}
+console.log("\n");
+//Ejercicio 3
+productos.forEach((producto) => {
+    console.log(`Producto: ${producto.nombre} - Precio: ${producto.precio}`);
+});
+
+//Parte 2
+//Ejercicio 4
