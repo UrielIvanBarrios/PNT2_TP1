a7dcfa495b278b6ee8818f06c2df71ccf9da1fd6
CeliacByte <barriosurielivan@gmail.com>
2025-04-10 21:43:47 -0300

    Actualizacion con parte 3, incorporacion de un ejercicio de la parte 2 que se habia omitido

--- index.js
+++ index.js
@@ -28,8 +28,27 @@ console.log(nombresProductos);
 console.log("\n");
 //Ejercicio 5
 const productosTipoRopa = productos.filter((producto) => producto.categoria === "Ropa");
-console.log(productosTipoRopa);
+console.log(`Los productos de la categoria ropa son: ${productosTipoRopa}`);
 console.log("\n");
 //Ejercicio 6
+const arrayProductosMayor3000 = productos.filter((producto) => producto.precio > 3000);
+console.log(`Los productos con precios mayores a $3000 son: ${arrayProductosMayor3000}`);
+console.log("\n");
+//Ejercicio 7
 const objetoGorra = productos.find((producto) => producto.nombre === "Gorra");
 console.log(objetoGorra);
+console.log("\n");
+
+//Parte 3
+//Ejercicio 8
+const precioMayor = productos.some((producto) => producto.precio > 10000);
+console.log(`Hay algun producto mayor a $10000: ${precioMayor}`); 
+console.log("\n");
+//Ejercicio 9
+const productosCuestanMas1000 = productos.every((producto) => producto.precio > 1000);
+console.log(`Los productos mas de $1000: ${productosCuestanMas1000}`);
+console.log("\n");
+//Ejercicio 10
+const comprobarProducto = nombresProductos.includes("Campera");
+console.log(`El producto Campera existe: ${comprobarProducto}`);
+console.log("\n");
