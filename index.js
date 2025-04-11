e663e8c1c3c252190c81a4b73ff3500f829139ca
CeliacByte <barriosurielivan@gmail.com>
2025-04-10 21:51:45 -0300

    Actualizacion con parte 4 y correcciones de visualizacion de arrays en consola

--- index.js
+++ index.js
@@ -28,11 +28,13 @@ console.log(nombresProductos);
 console.log("\n");
 //Ejercicio 5
 const productosTipoRopa = productos.filter((producto) => producto.categoria === "Ropa");
-console.log(`Los productos de la categoria ropa son: ${productosTipoRopa}`);
+console.log(`Los productos de la categoria ropa son: `);
+console.log(productosTipoRopa);
 console.log("\n");
 //Ejercicio 6
 const arrayProductosMayor3000 = productos.filter((producto) => producto.precio > 3000);
-console.log(`Los productos con precios mayores a $3000 son: ${arrayProductosMayor3000}`);
+console.log(`Los productos con precios mayores a $3000 son: `);
+console.log(arrayProductosMayor3000);
 console.log("\n");
 //Ejercicio 7
 const objetoGorra = productos.find((producto) => producto.nombre === "Gorra");
@@ -51,4 +53,28 @@ console.log("\n");
 //Ejercicio 10
 const comprobarProducto = nombresProductos.includes("Campera");
 console.log(`El producto Campera existe: ${comprobarProducto}`);
+console.log("\n");
+
+//Parte 4
+//Ejercicio 11
+const productosOrdenadosPorPrecio = productos.sort((a, b) => a.precio - b.precio);
+console.log(`Productos ordenados por precio de menor a mayor: `);  
+console.log(productosOrdenadosPorPrecio);
+console.log("\n");
+//Ejercicio 12
+const productosMensajePersonalizado = productos.map((producto) => {
+    return `El producto ${producto.nombre} cuesta $${producto.precio} y pertenece a la categoria ${producto.categoria}`;
+})
+console.log("Productos con mensaje personalizado: ");
+console.log(productosMensajePersonalizado);
+console.log("\n");
+//Ejercicio 13  
+const productos2 = [
+    { id: 6, nombre: "Medias", precio: 500, categoria: "Ropa" },
+    { id: 7, nombre: "Buzo", precio: 3500, categoria: "Ropa" },
+    ];
+
+const productosCombinados = [...productos, ...productos2];
+console.log("Productos combinados: ");
+console.log(productosCombinados);
 console.log("\n");
